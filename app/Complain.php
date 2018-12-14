a76430fb0300fc442c4d53773c8dd6b503d97a76
TheOnlySmartBoy <o2jose43@gmail.com>
2018-12-14 19:12:01 +0300

    Housekeeping

--- app/Complain.php
+++ app/Complain.php
@@ -5,8 +5,21 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Complain extends Model {
-    //Empty class body to be added later
-    //
-    //I dont know why it keeps deleting itself
-    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    //protected $fillable = [
+        //'name', 'email', 'password',
+    //];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    //protected $hidden = [
+        //'password', 'remember_token',
+    //];
 }
