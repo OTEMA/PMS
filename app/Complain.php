e207b35d61f5bb0d8b9e6374108d5fb50d26579a
TheOnlySmartBoy <o2jose43@gmail.com>
2018-12-14 14:28:59 +0300

    Finished adding various controller files

---
+++ app/Complain.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Complain extends Model
+{
+    //Empty calss body to be added later
+    //Through the subclass creataed in the controller
+}
